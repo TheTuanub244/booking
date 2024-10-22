9794f5e9976ed88f8b199b68e4681dae6b1abff5
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-10-22 20:36:56 +0700

    add api

--- be/src/main.ts
+++ be/src/main.ts
@@ -1,12 +1,11 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common/pipes/validation.pipe';
-import { RolesGuard } from './common/guards/roles.guard';
-import { JwtAuthGuard } from './common/guards/jwt-auth.guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
   await app.listen(8000);
 }
 bootstrap();

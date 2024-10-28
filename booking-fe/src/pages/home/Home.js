6b86cffadeb75423a8d5eb18d07f12e2bb960bee
minhtuanle301202 <Minhtuan8d@gmail.com>
2024-10-28 23:08:50 +0700

    create-easy-trip

--- booking-fe/src/pages/home/Home.js
+++ booking-fe/src/pages/home/Home.js
@@ -5,6 +5,9 @@ import './home.css';
 import PropertyList from '../../componets/propertyList/PropertyList';
 import FeaturedProperties from '../../componets/featuredProperties/FeaturedProperties';
 import Footer from '../../componets/footer/Footer';
+import EasyTrip from '../../componets/easyTrip/EasyTrip';
+import TredingDestination from '../../componets/tredingDestination/TredingDestination';
+import  RecentRearch  from '../../componets/recentResearch/RecentRearch';
 
 function Home() {
   const propetyByRates = async () => {
@@ -25,6 +28,8 @@ function Home() {
         <TredingDestination/>
         <h1 className='homeTitle'>Browse by property type</h1>
         <PropertyList />
+        <h1 className='homeTitle'>Quick and easy trip planner</h1>
+        <EasyTrip/>
         <h1 className='homeTitle'>Home guests love</h1>
         <FeaturedProperties/>
         <Footer/>

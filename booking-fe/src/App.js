1341e4dc052cbb1b8603bc615d8b62b7eca8ecb8
tuando0310 <154692218+tuando0310@users.noreply.github.com>
2024-11-17 07:02:35 +0700

    admin page ( dash board, user, room, chua call api, chua co add room add user, chua co thong ke)

--- booking-fe/src/App.js
+++ booking-fe/src/App.js
@@ -9,13 +9,14 @@ import Auth from "./pages/auth/Auth";
 import Property from "./pages/property/Property";
 import PartnerDashboard from "./pages/partner/partnerDashboard/partnerDashboard";
 import PartnerRegister from "./pages/partner/partnerRegister/partnerRegister";
+import AdminDashboard from "./pages/admin/AdminDashboard";
+import AddNewUser from "./pages/admin/contents/AddNewUser/AddNewUser";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />}></Route>
-
         <Route path="/" element={<Home />}></Route>
         <Route path="/hotels" element={<List />}></Route>
         <Route path="/hotels/:id" element={<Hotel />}></Route>
@@ -24,10 +25,13 @@ function App() {
         <Route path="/auth" element={<Auth />}></Route>
         <Route path="/property" element={<Property />}></Route>
         <Route path="/property/:id" element={<Property />}></Route>
-        <Route
-          path="/partner/partnerDashboard"
-          element={<PartnerDashboard />}
-        />
+        <Route path="/admin" element={<AdminDashboard />}>
+              <Route
+                path="new"
+                element={<AddNewUser/>}
+              />
+            </Route>
+        <Route path="/partner/partnerDashboard" element={<PartnerDashboard />} />
         <Route path="/partner/partnerRegister" element={<PartnerRegister />} />
       </Routes>
     </BrowserRouter>
---
+++ booking-fe/src/pages/admin/contents/AddNewUser/AddNewUser.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import SideBar from '../../component/SideBar/SideBar'
+import RightSide from '../../component/RightSide/RightSide'
+
+const AddNewUser = () => {
+  return (
+    <div>
+      <div className="AdminDashboard" >
+            <div className="DashboardGlass">
+                <SideBar/>
+                
+                <RightSide/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default AddNewUser

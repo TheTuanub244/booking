00a378016302927e32d19f36fd9c5487a68071d9
tuando0310 <154692218+tuando0310@users.noreply.github.com>
2024-12-01 15:21:23 +0700

    partner request

--- booking-fe/src/pages/admin/component/PartnerRequestTable/PartnerRequestTable.jsx
+++ booking-fe/src/pages/admin/component/PartnerRequestTable/PartnerRequestTable.jsx
@@ -1,93 +1,85 @@
+// PartnerRequestTable.jsx
+import React, { useState, useEffect } from "react";
 import "./PartnerRequestTable.css";
 import { DataGrid } from "@mui/x-data-grid";
-import { partnerRequestRows } from "../../data/partnerRequestData";
-import { Link } from "react-router-dom";
-import { useState } from "react";
 
-const PartnerRequestTable = () => {
-  const [data, setData] = useState(partnerRequestRows);
-  const handleDecline = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
-  const handleAcept = (id) => {
-    setData(data.filter((item) => item.id !== id));
-  };
-  function formatFriendlyDatetime(requestAt) {
-    const date = new Date(requestAt);
+const PartnerRequestTable = ({ partnerRequests, onAccept, onDecline }) => {
+  const [data, setData] = useState([]);
 
-    const options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      hour12: true,
-    };
+  useEffect(() => {
+    const tableRows = partnerRequests.map((partnerRequest) => ({
+      ...partnerRequest,
+      id: partnerRequest._id, 
+    }));
+    setData(tableRows);
+  }, [partnerRequests]);
 
-    return date.toLocaleString("en-US", options);
-  }
-
-  const userColumn = [
-    { field: "id", headerName: "ID", width: 50 },
+  const columns = [
     {
-      field: "user",
-      headerName: "User",
-      width: 130,
-      renderCell: (params) => {
-        return (
-          <div className="cellWithImg">
-            <img className="cellImg" src={params.row.img} alt="avatar" />
-            {params.row.username}
-          </div>
-        );
-      },
+      field: "userName",
+      headerName: "User Name",
+      width: 200,
+      renderCell: (params) => (
+        <div>
+          
+          {params.row.userName}
+        </div>
+      ),
     },
     {
       field: "email",
       headerName: "Email",
-      width: 200,
+      width: 250,
     },
-
     {
-      field: "age",
-      headerName: "Age",
-      width: 100,
+      field: "phoneNumber",
+      headerName: "Phone Number",
+      width: 150,
+    },
+    {
+      field: "address",
+      headerName: "Address",
+      width: 300,
+      valueGetter: (params) => {
+        const { province, district, ward } = params.row.address || {};
+        return `${province || ""}, ${district || ""}, ${ward || ""}`;
+      },
     },
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: (params) => {
-        return (
-          <div className="cellAction">
-            <div
-              className="acceptButton"
-              onClick={() => handleAcept(params.row.id)}
-            >
-              Accept
-            </div>
-            <div
-              className="declineButton"
-              onClick={() => handleDecline(params.row.id)}
-            >
-              Delete
-            </div>
+      sortable: false,
+      filterable: false,
+      renderCell: (params) => (
+        <div className="cellAction">
+      
+          <div
+            className="acceptButton"
+            onClick={() => onAccept(params.row.id)}
+          >
+            Accept
           </div>
-        );
-      },
+          <div
+            className="declineButton"
+            onClick={() => onDecline(params.row.id)}
+          >
+            Decline
+          </div>
+        </div>
+      ),
     },
   ];
+
   return (
-    <div className="datatable">
-      <div className="datatableTitle">Partner Request</div>
+    <div className="partnerRequestGrid">
       <DataGrid
-        className="datagrid"
+        className="partnerRequestGrid"
         rows={data}
-        columns={userColumn}
+        columns={columns}
         pageSize={9}
-        rowsPerPageOptions={[9]}
-        checkboxSelection
+        rowsPerPageOptions={[9, 15, 25]}
+        disableSelectionOnClick
       />
     </div>
   );

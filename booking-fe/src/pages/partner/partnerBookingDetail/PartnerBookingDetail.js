23e6a205d226e27800e565f199ed2f89cf495f33
minhtuanle301202 <Minhtuan8d@gmail.com>
2024-12-06 10:15:58 +0700

    add modal in partnerDetail

--- booking-fe/src/pages/partner/partnerBookingDetail/PartnerBookingDetail.js
+++ booking-fe/src/pages/partner/partnerBookingDetail/PartnerBookingDetail.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import Loading from "../../../componets/loading/Loading";
 import "./PartnerBookingDetail.css";
+import Modal from "react-modal";
 import {
   calculateNights,
   formatDateDayMonthAndYear,
@@ -13,9 +14,22 @@ const PartnerBookingDetail = () => {
   const location = useLocation();
   const [booking, setBooking] = useState();
   const [isExpanded, setIsExpanded] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const closeModal = () => setIsModalOpen(false);
+
+  const toggleModal = () => {
+    setIsModalOpen(true);
+  }
+
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
   };
+
+  const handleCancelBooking = () => {
+
+  }
+
+
   const [roomCounts, setRoomCounts] = useState();
   useEffect(() => {
     setBooking(location.state);
@@ -70,7 +84,7 @@ const PartnerBookingDetail = () => {
                 Booking Status: {booking.booking_status}
               </p>
 
-              <button className="cancelBookingBtn">Cancel Booking</button>
+              <button className="cancelBookingBtn" onClick={toggleModal}>Cancel Booking</button>
             </div>
 
 
@@ -222,7 +236,75 @@ const PartnerBookingDetail = () => {
               </div>
             </div>
           </div>
+          <Modal
+            isOpen={isModalOpen}
+            onRequestClose={closeModal}
+            style={{
+              overlay: {
+                backgroundColor: "rgba(0, 0, 0, 0.5)",
+              },
+              content: {
+                top: "50%",
+                left: "50%",
+                right: "auto",
+                bottom: "auto",
+                marginRight: "-50%",
+                transform: "translate(-50%, -50%)",
+                width: "550px",
+                textAlign: "center",
+                borderRadius: "10px",
+              },
+            }}
+          >
+            {booking.booking_status === "Completed" ? (
+              <div>
+                <h3>Bạn có muốn hủy chuyến đi không?
+                </h3>
+                <h4>Nếu đồng ý chúng tôi sẽ gửi email để xác nhận</h4>
+              </div>
+            ) : (
+              <div>
+                <h3>Bạn có muốn hủy chuyến đi không?
+                </h3>
+              </div>
+            )}
+            
+            <div style={{ marginTop: "20px" }}>
+              <button
+                style={{
+                  marginRight: "10px",
+                  padding: "10px 20px",
+                  backgroundColor: "#007BFF",
+                  color: "#fff",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+                onClick={handleCancelBooking}
+              >
+                Đồng ý
+              </button>
+              <button
+                style={{
+                  padding: "10px 20px",
+                  backgroundColor: "#6c757d",
+                  color: "#fff",
+                  border: "none",
+                  borderRadius: "5px",
+                  cursor: "pointer",
+                }}
+                onClick={closeModal}
+              >
+                Không
+              </button>
+            </div>
+          </Modal>
+
         </div>
+
+
+
+
       ) : (
         <Loading />
       )}

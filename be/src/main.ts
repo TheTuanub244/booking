0863ac53cea25146b51373fddbade015fb9a2cca
TheTuanub244 <tuan.mt211010@sis.hust.edu.vn>
2024-12-09 20:29:04 +0700

    add

--- be/src/main.ts
+++ be/src/main.ts
@@ -12,7 +12,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://maithetuan-20211010.web.app',
+    );
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header(
       'Access-Control-Allow-Headers',
@@ -23,7 +26,7 @@ async function bootstrap() {
   });
   app.enableCors({
     origin: [
-      'https://maithetuan-20211010.web.app/',
+      'https://maithetuan-20211010.web.app',
       'http://localhost:3000',
       'http://192.169.101.137:3000',
     ],
